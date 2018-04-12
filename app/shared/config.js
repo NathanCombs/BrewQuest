9df8526c0b1fff8d7de1b297d5b3609a5ac2fe29
Nathan Combs <ncombs1152@gmail.com>
2018-04-12 13:54:13 -0600

    server functional: no connection to front end yet

---
+++ app/shared/config.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Config = /** @class */ (function () {
+    function Config() {
+    }
+    Config.apiUrl = "https://baas.kinvey.com/";
+    Config.appKey = "kid_HyHoT_REf";
+    Config.authHeader = "Basic a2lkX0h5SG9UX1JFZjo1MTkxMDJlZWFhMzQ0MzMyODFjN2MyODM3MGQ5OTIzMQ";
+    Config.token = "";
+    return Config;
+}());
+exports.Config = Config;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiY29uZmlnLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiY29uZmlnLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiI7O0FBQUE7SUFBQTtJQUtFLENBQUM7SUFKUSxhQUFNLEdBQUcsMEJBQTBCLENBQUM7SUFDcEMsYUFBTSxHQUFHLGVBQWUsQ0FBQztJQUN6QixpQkFBVSxHQUFHLHNFQUFzRSxDQUFDO0lBQ3BGLFlBQUssR0FBRyxFQUFFLENBQUM7SUFDcEIsYUFBQztDQUFBLEFBTEgsSUFLRztBQUxVLHdCQUFNIiwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGNsYXNzIENvbmZpZyB7XG4gICAgc3RhdGljIGFwaVVybCA9IFwiaHR0cHM6Ly9iYWFzLmtpbnZleS5jb20vXCI7XG4gICAgc3RhdGljIGFwcEtleSA9IFwia2lkX0h5SG9UX1JFZlwiO1xuICAgIHN0YXRpYyBhdXRoSGVhZGVyID0gXCJCYXNpYyBhMmxrWDBoNVNHOVVYMUpGWmpvMU1Ua3hNREpsWldGaE16UTBNek15T0RGak4yTXlPRE0zTUdRNU9USXpNUVwiO1xuICAgIHN0YXRpYyB0b2tlbiA9IFwiXCI7XG4gIH0iXX0=
---
+++ server/server.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+var express = require("express");
+var app = express();
+var bcrypt = require("bcrypt");
+const saltRounds = 10;
+var jwt = require("jsonwebtoken");
+var path = require("path");
+var pg = require("pg");
+var bodyParser = require("body-parser");
+var conString = `postgres://${process.env.DB_USER}:${process.env.DB_PASS}@baasu.db.elephantsql.com:5432/${process.env.DB_USER}`;
+
+app.use(express.static(path.join(__dirname, "build")));
+app.use(bodyParser.json({ type: 'application/json' }));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var client = new pg.Client(conString);
+
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  } else {
+      console.log("DB connection successful");
+      app.listen(process.env.PORT || 5000, () => {
+          console.log(`Server started on port ${process.env.PORT || 5000}`)
+      })
+  }
+});
